37d25c0e8c776505bf0c3e04e29c4b48757ca69a
propes <james.propert@gmail.com>
2021-06-18 15:35:59 +0100

    Add code

--- FilenameSanitizer/FilenameSanitizer.cs
+++ FilenameSanitizer/FilenameSanitizer.cs
@@ -3,7 +3,26 @@
 namespace FilenameSanitizer
 {
     public class FilenameSanitizer
-    {
-        // Add code
-    }
+	{
+		public string RemoveIllegalCharacters(string filename)
+		{
+			if (string.IsNullOrEmpty(filename))
+			{
+				throw new ArgumentException("Filename must not be null or empty", nameof(filename));
+			}
+
+			var invalidChars = Path.GetInvalidFileNameChars();
+			var sb = new StringBuilder();
+
+			foreach (var @char in filename)
+			{
+				if (!invalidChars.Contains(@char))
+				{
+					sb.Append(@char);
+				}
+			}
+
+			return sb.ToString();
+		}
+	}
 }
